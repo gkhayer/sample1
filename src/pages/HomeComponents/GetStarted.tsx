fc47fa74d6427d8d66f94c8c1e9cba535a3c5488
gkhayer <gagankaur.hayer@gmail.cm>
2024-07-08 17:55:18 -0700

    fix  month/ yearly button width

--- src/pages/HomeComponents/GetStarted.tsx
+++ src/pages/HomeComponents/GetStarted.tsx
@@ -58,16 +58,16 @@ const GetStarted = () => {
   return (
     <div>
       <h6 className="b text-5xl">Pricing</h6>
-      <p>
+      <p className="text-xl text-left">
         Dive into the possiblities with our creative offerings. These
         meticulously crafted service packages go beyond traditional pricing
         plans, offering a spectrum of innovation solutions to elevate you
         digital presence.
       </p>
 
-      <div className="border-2 p-1 m-1 inline-block">
-        <button className="m-1">Monthly</button>
-        <button className="m-1">Yearly</button>
+      <div className="border-2 p-1 m-5 inline-block">
+        <button className="m-1 px-5 w-30">Monthly</button>
+        <button className="m-1 px-5 w-30">Yearly</button>
       </div>
 
       <div>
